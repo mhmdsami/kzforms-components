df31ac35b2b99442ef85bf075b556b933c91593a
Mohamed Sami <srmf2025@gmail.com>
2022-12-13 12:43:11 +0530

    :sparkles: add runtimeConfig and move form data to api

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   pages: true,
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_APP_API_URL || "http://localhost:5000",
+    },
+  },
   css: ["~/assets/styles/global.scss"],
   postcss: {
     config: false,
--- pages/form/[id].vue
+++ pages/form/[id].vue
@@ -8,40 +8,20 @@
 </template>
   
 <script setup lang="ts">
+
 import { Form } from "~/types/global";
+
+const config = useRuntimeConfig();
 
 const route = useRoute();
 const id = route.params.id;
 
-const forms: Form[] = [
-  {
-    id: "UfvnFGGEwu",
-    name: "Registration Form",
-    elements: [{ id: 1, type: "TextField", attr: { label: "Name", placeholder: "What should we call you?" } },
-    { id: 2, type: "TextField", attr: { label: "Registration Number", placeholder: "Your unique identification!" } },
-    { id: 3, type: "Button", attr: { content: "Lets Go!" } }],
-    theme: {
-      id: "1",
-      primary: "#CED3DC",
-      secondary: "#95B46A"
-    }
-  },
-  {
-    id: "rPFdvPJTOo",
-    name: "Feedback Form",
-    elements: [{ id: 1, type: "TextField", attr: { label: "Name", placeholder: "Your Name!" } },
-    { id: 2, type: "TextField", attr: { label: "Feedback", placeholder: "How did it for for you!" } },
-    { id: 3, type: "TextField", attr: { label: "Rate it", placeholder: "Rate the speaker!" } },
-    { id: 4, type: "Button", attr: { content: "Send!" } }],
-    theme: {
-      id: "1",
-      primary: "#C2C5BB",
-      secondary: "#77B28C"
-    }
-  }
-]
+const { data } = await useFetch(`${config.public.apiBase}/form/${id}`);
+const form = data.value as Form;
 
-const form = forms.find((form) => form.id === id);
+if (!form) {
+  throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
+}
 
 onBeforeMount(() => {
   document.documentElement.style.setProperty("--primary-color", form.theme.primary);
